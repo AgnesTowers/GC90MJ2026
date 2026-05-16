6bde02144566940a13873763005f254a3b22ac78
Eskarina Ulrich <eskarina.ulrich@1und.de>
2026-05-16 15:54:03 +0200

    make mirroe effect offset

--- effects/mirror_effect_color_rect.gd
+++ effects/mirror_effect_color_rect.gd
@@ -1,6 +1,7 @@
 extends ColorRect
 
 @export var target: Node2D
+@export var offset: Vector2i = Vector2i(-100, 0)
 
 func _process(delta: float) -> void:
-	global_position.x = target.global_position.x
+	global_position.x = target.global_position.x + offset.x
