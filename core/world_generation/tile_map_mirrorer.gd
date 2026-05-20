186af3792227cc08669e633c9df2ce3b3c9ed04e
Eskarina Ulrich <eskarina.ulrich@1und.de>
2026-05-20 13:38:08 +0200

    make tile map mirrorer less crap

--- core/world_generation/tile_map_mirrorer.gd
+++ core/world_generation/tile_map_mirrorer.gd
@@ -1,19 +1,38 @@
 @tool
-extends Node
+extends Node2D
 class_name TileMapLayerMirror
 
-@export var source_tile_map_layer: TileMapLayer
-@export var target_tile_map_layer: TileMapLayer
-@export var mirror_plane_y: int = 0
+@export var source_tile_map_layers: Array[TileMapLayer]
+@export var mirror_offest: Vector2 = Vector2(0, 0)
 
-var tile_mirror_mapping: Dictionary[int, int] = {0: 2, 1: 1,2: 0 }
 
-@export_tool_button("Mirror TileMapLayer") var mirror_action = mirror_tile_map
+@export_tool_button("Mirror TileMapLayers") var mirror_action = mirror_tile_maps
+@export_tool_button("Clear TileMapLayer") var clear_action = free_tile_maps
 
-func mirror_tile_map() -> void:
+
+func mirror_tile_maps() -> void:
 	
-	target_tile_map_layer.tile_set = source_tile_map_layer.tile_set
-	target_tile_map_layer.tile_map_data = source_tile_map_layer.tile_map_data
-	target_tile_map_layer.scale.y = - source_tile_map_layer.scale.y
+	free_tile_maps()
 	
-	pass
+	for tile_map_layer in source_tile_map_layers:
+	
+		var new_tile_map_layer = TileMapLayer.new()
+	
+		new_tile_map_layer.tile_set = tile_map_layer.tile_set
+		new_tile_map_layer.tile_map_data = tile_map_layer.tile_map_data
+		new_tile_map_layer.scale.y = - tile_map_layer.scale.y
+		
+		new_tile_map_layer.position.x = new_tile_map_layer.tile_set.tile_size.x * mirror_offest.x
+		new_tile_map_layer.position.y = new_tile_map_layer.tile_set.tile_size.y * mirror_offest.y
+		
+		add_child(new_tile_map_layer, true)
+		new_tile_map_layer.owner = get_tree().edited_scene_root
+		
+		tile_map_layer.changed.connect(func() -> void:
+			print(0)
+			)
+
+func free_tile_maps() -> void:
+	for child in get_children():
+		if child is TileMapLayer:
+			child.free()
